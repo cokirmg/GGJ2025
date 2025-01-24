b61312499906f4e7fa8c6215a6683c7776cbd059
cokirmg <100722843+cokirmg@users.noreply.github.com>
2025-01-25 00:17:28 +0100

    Movement complited

--- Assets/Rodri/PlayerMovement.cs
+++ Assets/Rodri/PlayerMovement.cs
@@ -4,15 +4,36 @@ public class PlayerMovement : MonoBehaviour
 {
     
     public CharacterController2D controller;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
+    
+    public float runSpeed = 40f;
+    
+    float horizontalMove = 0f;
+    bool jump = false;
+    bool crouch = false;
+    
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        }
+        else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
     }
 }
